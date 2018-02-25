a2f4f74ab383ac5d9aeb35e8d2fa0c1c259b7fea
Niloc92 <prestonresult@github.com>
2018-02-25 01:11:06 -0800

    worked Lesson Three

--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -1,59 +1,54 @@
 // Do not change any of the function names
 
 function returnFirst(arr) {
-  // return the first item from the array
+let i = arr[0];
+return i;
 }
 
 function returnLast(arr) {
-  // return the last item of the array
+  i=arr.pop();
+  return i;
 }
 
 function getArrayLength(arr) {
-  // return the length of the array
-}
+return arr.getArrayLength;}
 
 function incrementByOne(arr) {
-  // arr is an array of integers  
-  // increase each integer by one
-  // return the array
+ for(i = 0 ; i <arr.length;i++){
+   arr[i]=arr[i]+1;
+ }
+ return arr;
 }
 
 function addItemToArray(arr, item) {
-  // add the item to the end of the array
-  // return the array
+  arr.push(item)
+  return arr
 }
 
 function addItemToFront(arr, item) {
-  // add the item to the front of the array
-  // return the array
-  // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 }
 
 function wordsToSentence(words) {
-  // words is an array of strings
-  // return a string that is all of the words concatenated together
-  // spaces need to be between each word
-  // example: ['Hello', 'world!'] -> 'Hello world!'
+  return words.join('')
 }
 
 function contains(arr, item) {
-  // check to see if item is inside of arr
-  // return true if it is, otherwise return false
+return arr.includes(item);
 }
 
 function addNumbers(numbers) {
-  // numbers is an array of integers.
-  // add all of the integers and return the value
-}
+  return arr['']+1;
 
 function averageTestScore(testScores) {
-  // testScores is an array.  Iterate over testScores and compute the average.
-  // return the average
+  let sum = numbers.reduce((a,b)=>a+b,0);
+  return sum;
 }
 
 function largestNumber(numbers) {
-  // numbers is an array of integers
-  // return the largest integer
+ let i = Math.max(...numbers);
+ return i;
 }
 
 // Do not modify code below this line.
